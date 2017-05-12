7e44a24292fb901ec47e0f6e8870adb3377f2987
chen_zhihui <chen_zhihui@cdv.com>
2017-05-12 19:03:51 +0800

    ioc

---
+++ c++/cpp11/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <functional>
+#include <string>
+
+using namespace std;
+
+
+template<typename Fun, typename ... Args>
+void fun(Fun f, Args && ... args) {
+    cout << sizeof...(args)<<endl;
+    f(std::forward<Args>(args) ... );
+}
+
+void foo(int i , int j ) {
+    cout<<i <<"+"<<j << endl;
+}
+
+void bar(const std::string name) {
+    cout << name;
+}
+
+int main(int argc, char *argv[])
+{
+//    cout << "Hello World!" << endl;
+
+    fun(foo, 4, 5);
+    fun(bar,std::string("str"));
+
+    return 0;
+}
